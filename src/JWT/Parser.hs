8f64f2c5f93200a798ecc3ec14e6f1d880ebba16
feuvpi <fredvpg@gmail.com>
2025-05-07 23:01:02 -0300

    .

--- src/JWT/Parser.hs
+++ src/JWT/Parser.hs
@@ -6,11 +6,13 @@ module JWT.Parser
   , JWTPayload(..)
   , JWTClaim(..)
   , ValidationError(..)
+  , DecodedJWT(..)
   ) where
 
 import Data.List.Split (splitOn)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Lazy.Char8 as BLC
+import qualified Data.ByteString as BS
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified Data.Aeson as A
@@ -18,10 +20,16 @@ import qualified Data.Map.Strict as Map
 import qualified Data.Text.Lazy.Encoding as TLE
 import qualified Data.Text.Lazy as TL
 import qualified Data.ByteString.Base64 as B64
+
+
+
 import Data.Maybe (fromMaybe)
+import Data.Char (ord)
 
-
-import Data.List.Split (splitOn)
+import Data.Aeson.Types (parseMaybe)
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Aeson.Key as Key
+import qualified Data.Aeson.KeyMap as KM
 
 -- | Represents the three parts of a JWT token
 data JWTParts = JWTParts 
@@ -30,7 +38,7 @@ data JWTParts = JWTParts
   , jwtSignature :: String
   } deriving (Show, Eq)
 
-  -- | JWT Header structure
+-- | JWT Header structure
 data JWTHeader = JWTHeader
   { jwsAlg :: Maybe String  -- ^ Algorithm used
   , jwsTyp :: Maybe String  -- ^ Token type
@@ -38,7 +46,7 @@ data JWTHeader = JWTHeader
   , jwsOther :: Map.Map String A.Value  -- ^ Other header fields
   } deriving (Show, Eq)
 
-  -- | JWT Claim value
+-- | JWT Claim value
 data JWTClaim = 
     StringClaim String
   | NumberClaim Double
@@ -48,7 +56,7 @@ data JWTClaim =
   | MapClaim (Map.Map String JWTClaim)
   deriving (Show, Eq)
 
-  -- | JWT Payload structure
+-- | JWT Payload structure
 data JWTPayload = JWTPayload
   { jwtIss :: Maybe String  -- ^ Issuer
   , jwtSub :: Maybe String  -- ^ Subject
@@ -60,7 +68,7 @@ data JWTPayload = JWTPayload
   , jwtClaims :: Map.Map String JWTClaim  -- ^ Custom claims
   } deriving (Show, Eq)
 
-  -- | Decoded JWT structure
+-- | Decoded JWT structure
 data DecodedJWT = DecodedJWT
   { decodedHeader :: JWTHeader
   , decodedPayload :: JWTPayload
@@ -75,15 +83,12 @@ data ValidationError =
   | MissingRequiredClaim String
   deriving (Show, Eq)
 
-
-
 -- | Parse a JWT token string into its component parts
 parseJWT :: String -> Either ValidationError JWTParts
 parseJWT token = 
   case splitOn "." token of
     [h, p, s] -> Right $ JWTParts h p s
     _ -> Left $ InvalidTokenFormat "Token must contain exactly two dots separating three components"
-
 
 -- | Decode a JWT token into its structured components
 decodeJWT :: String -> Either ValidationError DecodedJWT
@@ -92,7 +97,6 @@ decodeJWT token = do
   header <- decodeHeader (jwtHeader parts)
   payload <- decodePayload (jwtPayload parts)
   return $ DecodedJWT header payload (jwtSignature parts)
-
 
 -- | Decode the JWT header
 decodeHeader :: String -> Either ValidationError JWTHeader
@@ -116,11 +120,11 @@ decodeBase64Url str =
   let 
     -- Replace base64url specific chars and add padding
     normalized = fixBase64Padding $ T.unpack $ T.replace (T.pack "-") (T.pack "+") $ T.replace (T.pack "_") (T.pack "/") $ T.pack str
+    bytes = BS.pack $ map (fromIntegral . ord) normalized
   in
-    case B64.decode (BLC.pack normalized) of
-      Left err -> Left $ InvalidBase64 $ "Failed to decode base64: " ++ show err
+    case B64.decode bytes of
+      Left err -> Left $ InvalidBase64 $ "Failed to decode base64: " ++ err
       Right bs -> Right $ BL.fromStrict bs
-
 
 -- | Fix base64 padding
 fixBase64Padding :: String -> String
@@ -131,26 +135,39 @@ fixBase64Padding s =
      then s
      else s ++ replicate (4 - remainder) '='
 
+-- | Extract string value from Aeson Value
+extractString :: A.Value -> Maybe String
+extractString (A.String s) = Just (T.unpack s)
+extractString _ = Nothing
+
+-- | Extract number value from Aeson Value as Integer
+extractInteger :: A.Value -> Maybe Integer
+extractInteger (A.Number n) = Just (round $ realToFrac n)
+extractInteger _ = Nothing
+
+-- | Extract number value from Aeson Value as Double
+extractDouble :: A.Value -> Maybe Double
+extractDouble (A.Number n) = Just (realToFrac n)
+extractDouble _ = Nothing
+
+-- | Convert Aeson Object to Map
+aesonObjectToMap :: A.Object -> Map.Map String A.Value
+aesonObjectToMap o = Map.fromList [(T.unpack $ Key.toText k, v) | (k, v) <- KM.toList o]
 
 -- | Parse header JSON into a JWTHeader structure
 parseHeaderJson :: A.Value -> JWTHeader
 parseHeaderJson (A.Object o) =
   let 
-    alg = case A.lookup "alg" o of
-            Just v -> A.decode (A.encode v)
-            Nothing -> Nothing
-    typ = case A.lookup "typ" o of
-            Just v -> A.decode (A.encode v)
-            Nothing -> Nothing 
-    kid = case A.lookup "kid" o of
-            Just v -> A.decode (A.encode v)
-            Nothing -> Nothing
+    oMap = aesonObjectToMap o
+    alg = Map.lookup "alg" oMap >>= extractString
+    typ = Map.lookup "typ" oMap >>= extractString
+    kid = Map.lookup "kid" oMap >>= extractString
+    
     -- Filter out the known fields and keep the rest
-    otherFields = Map.fromList $ filter (\(k, _) -> k /= "alg" && k /= "typ" && k /= "kid") $ A.toList o
+    otherFields = Map.filterWithKey (\k _ -> k /= "alg" && k /= "typ" && k /= "kid") oMap
   in
     JWTHeader alg typ kid otherFields
 parseHeaderJson _ = JWTHeader Nothing Nothing Nothing Map.empty
-
 
 -- | Parse claim value
 parseClaimValue :: A.Value -> JWTClaim
@@ -158,52 +175,29 @@ parseClaimValue (A.String s) = StringClaim (T.unpack s)
 parseClaimValue (A.Number n) = NumberClaim (realToFrac n)
 parseClaimValue (A.Bool b) = BoolClaim b
 parseClaimValue A.Null = NullClaim
-parseClaimValue (A.Array arr) = ListClaim (map parseClaimValue arr)
-parseClaimValue (A.Object obj) = MapClaim (Map.map parseClaimValue obj)
-
+parseClaimValue (A.Array arr) = ListClaim $ map parseClaimValue $ foldr (:) [] arr
+parseClaimValue (A.Object obj) = MapClaim $ Map.map parseClaimValue $ aesonObjectToMap obj
 
 -- | Parse payload JSON into a JWTPayload structure
 parsePayloadJson :: A.Value -> JWTPayload
 parsePayloadJson (A.Object o) =
   let 
-    -- Go back to the original case expressions which worked
-    iss = case Map.lookup "iss" o of
-            Just v -> A.decode (A.encode v) 
-            Nothing -> Nothing
-    sub = case Map.lookup "sub" o of
-            Just v -> A.decode (A.encode v)
-            Nothing -> Nothing
-    aud = case Map.lookup "aud" o of
-            Just v -> A.decode (A.encode v)
-            Nothing -> Nothing
-    exp = case Map.lookup "exp" o of
-            Just v -> A.decode (A.encode v)
-            Nothing -> Nothing
-    nbf = case Map.lookup "nbf" o of
-            Just v -> A.decode (A.encode v)
-            Nothing -> Nothing
-    iat = case Map.lookup "iat" o of
-            Just v -> A.decode (A.encode v)
-            Nothing -> Nothing
-    jti = case Map.lookup "jti" o of
-            Just v -> A.decode (A.encode v)
-            Nothing -> Nothing
+    oMap = aesonObjectToMap o
+    
+    iss = Map.lookup "iss" oMap >>= extractString
+    sub = Map.lookup "sub" oMap >>= extractString
+    aud = Map.lookup "aud" oMap >>= extractString
+    exp = Map.lookup "exp" oMap >>= extractInteger
+    nbf = Map.lookup "nbf" oMap >>= extractInteger
+    iat = Map.lookup "iat" oMap >>= extractInteger
+    jti = Map.lookup "jti" oMap >>= extractString
     
     -- Filter out the standard claims and parse the remaining custom claims
     standardClaims = ["iss", "sub", "aud", "exp", "nbf", "iat", "jti"]
     customClaims = Map.fromList 
                  $ map (\(k, v) -> (k, parseClaimValue v)) 
                  $ filter (\(k, _) -> k `notElem` standardClaims) 
-                 $ Map.toList o
+                 $ Map.toList oMap
   in
     JWTPayload iss sub aud exp nbf iat jti customClaims
 parsePayloadJson _ = JWTPayload Nothing Nothing Nothing Nothing Nothing Nothing Nothing Map.empty
-
-
-
-
-
-
-
-
-
--- test/JWT/ParserSpec.hs
+++ test/JWT/ParserSpec.hs
@@ -1,19 +1,24 @@
-module JWT.ParserSpec where
+module JWT.ParserSpec (spec) where
 
 import Test.Hspec
 import JWT.Parser
 
 spec :: Spec
 spec = do
-  describe "parseJWT" $ do
-    it "should parse a valid JWT token" $ do
-      let token = "header.payload.signature"
-      parseJWT token `shouldBe` Right (JWTParts "header" "payload" "signature")
-    
-    it "should return an error for invalid tokens" $ do
-      let invalid = "header.payload"
-      parseJWT invalid `shouldBe` Left "Invalid JWT token format: must contain exactly two dots separating three components"
-
+  describe "JWT.Parser" $ do
+    describe "parseJWT" $ do
+      it "parses a valid JWT token" $ do
+        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+        case parseJWT token of
+          Left _ -> expectationFailure "Failed to parse valid JWT token"
+          Right parts -> do
+            jwtHeader parts `shouldBe` "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+            jwtPayload parts `shouldBe` "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ"
+            jwtSignature parts `shouldBe` "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
       
-
-
+      it "returns an error for an invalid JWT token" $ do
+        let invalid = "invalid"
+        case parseJWT invalid of
+          Left (InvalidTokenFormat _) -> return ()
+          Left _ -> expectationFailure "Expected InvalidTokenFormat error but got different error"
+          Right _ -> expectationFailure "Expected error but parsing succeeded"
